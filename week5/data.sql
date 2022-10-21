89f4c9bc14f136ff8c831f60a795f5b93129aa85
zoan57 <105167620+zoan57@users.noreply.github.com>
2022-10-21 16:07:59 +0800

    week5-SQL-v2.
    renew mysqldump

--- week5/data.sql
+++ week5/data.sql
@@ -1,77 +1,84 @@
-SHOW DATABASES;
-SHOW TABLES;
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (arm64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
-#要求⼆：建立資料庫和資料表
-CREATE DATABASE website;
-USE website;
-CREATE TABLE member(
-	id BIGINT PRIMARY KEY auto_increment COMMENT '獨立編號',
-    name VARCHAR(255) NOT null COMMENT '姓名',
-	username VARCHAR(255) NOT null COMMENT '帳戶名稱',
-    password VARCHAR(255) NOT null COMMENT '帳戶密碼',
-    follower_count int unsigned NOT null DEFAULT 0 COMMENT '追蹤者數量',
-    time datetime NOT null default current_timestamp COMMENT '註冊時間'
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-#使⽤ INSERT 指令新增資料到 member 資料表中
-INSERT INTO member(name, username, password) VALUES('unknown', 'test', 'test');
-INSERT INTO member(name, username, password) VALUES('Andrew', 'andrew', 'andrew123');
-INSERT INTO member(name, username, password) VALUES('Inu', 'lokah', 'lokah123');
-INSERT INTO member(name, username, password) VALUES('Pangcah', 'amis', 'amis123');
-INSERT INTO member(name, username, password) VALUES('viva', 'la', 'vida');
+--
+-- Table structure for table `member`
+--
 
-#使⽤ SELECT 指令取得所有在 member 資料表中的會員資料
-SELECT * FROM member;
-#使⽤ SELECT 指令取得所有在 member 資料表中的會員資料，並按照 time 欄位，由近到遠排序。
-SELECT * FROM member ORDER BY time ASC;
-#使⽤ SELECT 指令取得 member 資料表中第 2 ~ 4 共三筆資料，並按照 time 欄位，由近到遠排序。 ( 並非編號 2、3、4 的資料，⽽是排序後的第 2 ~ 4 筆資料 )
-SELECT * FROM member ORDER BY time ASC limit 1,3;
-#使⽤ SELECT 指令取得欄位 username 是 test的資料
-SELECT * FROM member WHERE username='test';
-#使⽤ SELECT 指令取得欄位 username 是 test、且欄位 password 也是 test 的資料
-SELECT * FROM member WHERE username='test' and password='test';
-#使⽤ UPDATE 指令更新欄位 username 是 test 的會員資料，將資料中的 name 欄位改成 test2
-SET SQL_SAFE_UPDATES=0;
-UPDATE member SET name='tes2' WHERE username='test';
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `name` varchar(255) NOT NULL COMMENT '姓名',
+  `username` varchar(255) NOT NULL COMMENT '帳戶名稱',
+  `password` varchar(255) NOT NULL COMMENT '帳戶密碼',
+  `follower_count` int unsigned NOT NULL DEFAULT '0' COMMENT '追蹤者數量',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '註冊時間',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-#要求四：SQL Aggregate Functions
-#取得 member 資料表中，總共有幾筆資料(幾位會員)
-SELECT COUNT(name) FROM member;
-#取得 member 資料表中，所有會員 follower_count 欄位的總和
-SELECT SUM(follower_count) FROM member;
-#取得 member 資料表中，所有會員 follower_count 欄位的平均數
-SELECT AVG(follower_count) FROM member;
+--
+-- Dumping data for table `member`
+--
 
-#要求五：SQL JOIN
-CREATE TABLE message(
-	id BIGINT PRIMARY KEY auto_increment COMMENT '獨立編號',
-	member_id BIGINT NOT null COMMENT '留言者會員編號',
-    content VARCHAR(255) NOT null COMMENT '留言內容',
-	like_count int unsigned NOT null DEFAULT 0 COMMENT '按讚的數量',
-    time datetime NOT null default current_timestamp COMMENT '留言時間'
-);
-ALTER TABLE message ADD FOREIGN KEY(member_id) REFERENCES member(id);
-INSERT INTO message(member_id, content) VALUES('1','哇好讚');
-INSERT INTO message(member_id, content) VALUES('2','安安');
-INSERT INTO message(member_id, content) VALUES('4','Test Test');
-INSERT INTO message(member_id, content) VALUES('3','ctlorem200');
-INSERT INTO message(member_id, content) VALUES('1','try try see');
-INSERT INTO message(member_id, content) VALUES('5','The making of indebted man');
-INSERT INTO message(member_id, content) VALUES('3','我愛半路咖啡');
-INSERT INTO message(member_id, content) VALUES('1','掰掰台北哈囉花蓮');
-INSERT INTO message(member_id, content) VALUES('2','我是機器人');
-INSERT INTO message(member_id, content) VALUES('4','最近想看雲圖，但最愛Ursula Le Guinㄌ');
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'tes2','test','test',0,'2022-10-21 11:50:56'),(2,'Andrew','andrew','andrew123',0,'2022-10-21 12:05:48'),(3,'Inu','lokah','lokah123',0,'2022-10-21 12:05:51'),(4,'Pangcah','amis','amis123',0,'2022-10-21 12:05:54'),(5,'viva','la','vida',0,'2022-10-21 12:05:56');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
 
-#確定有JOIN成功
-SELECT * FROM member INNER JOIN message ON member.id=message.member_id ORDER BY member.id;
-#使⽤ SELECT 搭配 JOIN 語法，取得所有留⾔，結果須包含留⾔者會員的姓名
-SELECT member.name, message.content FROM member
-INNER JOIN message ON member.id=message.member_id ORDER BY member.id;
+--
+-- Table structure for table `message`
+--
 
-#使⽤ SELECT 搭配 JOIN 語法，取得 member 資料表中欄位 username 是 test 的所有留⾔，資料中須包含留⾔者會員的姓名。
-SELECT member.name, message.content FROM member
-INNER JOIN message ON member.id=message.member_id WHERE member.username='test' ORDER BY member.id;
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `member_id` bigint NOT NULL COMMENT '留言者會員編號',
+  `content` varchar(255) NOT NULL COMMENT '留言內容',
+  `like_count` int unsigned NOT NULL DEFAULT '0' COMMENT '按讚的數量',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '留言時間',
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-#使⽤ SELECT、SQL Aggregate Functions 搭配 JOIN 語法，取得 member 資料表中欄位 username 是 test 的所有留⾔平均按讚數。
-SELECT member.username, FLOOR(AVG(message.like_count)) FROM member
-INNER JOIN message ON member.id=message.member_id WHERE member.username='test'  ORDER BY member.id;
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,4,'最近想看雲圖，但最愛Ursula Le Guinㄌ',0,'2022-10-21 14:33:03'),(2,1,'哇好讚',0,'2022-10-21 14:33:44'),(3,2,'安安',0,'2022-10-21 14:33:53'),(4,4,'Test Test',0,'2022-10-21 14:33:56'),(5,3,'ctlorem200',0,'2022-10-21 14:33:58'),(6,1,'try try see',0,'2022-10-21 14:33:59'),(7,5,'The making of indebted man',0,'2022-10-21 14:33:59'),(8,3,'我愛半路咖啡',0,'2022-10-21 14:34:00'),(9,1,'掰掰台北哈囉花蓮',0,'2022-10-21 14:34:01'),(10,2,'我是機器人',0,'2022-10-21 14:34:03');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-21 16:01:24
