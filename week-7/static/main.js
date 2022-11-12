d4dad6075ba76e57afe62d966a5f56e33fa1f37e
zoan57 <105167620+zoan57@users.noreply.github.com>
2022-11-12 20:46:30 +0800

    week7-update-v1

---
+++ week-7/static/main.js
@@ -0,0 +1,37 @@
+function serachUser() {
+    let username = document.getElementById("searchName").value;
+    console.log(username);
+    fetch("http://127.0.0.1:3000/api/member?username=" + username).then(function(response) {
+        return response.json();
+    }).then(function(value) {
+        document.getElementById("resultName").innerHTML = value.data.name + "(" + value.data.username + ")";
+    }).catch(function(error) {
+        document.getElementById("resultName").innerHTML = "User not found!"
+    })
+}
+
+function updateUser() {
+    let name = document.getElementById("updateName").value;
+    console.log(name);
+    fetch("http://127.0.0.1:3000/api/member", {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application/json",
+                "accept": "application/json"
+            },
+            body: JSON.stringify({
+                name: name
+            })
+        })
+        .then(function(response) {
+            return response.json();
+        }).then(function(value) {
+            document.getElementById("updateResult").innerHTML = "更新成功";
+        }).catch(function(error) {
+            document.getElementById("updateResult").innerHTML = "更新失敗"
+        })
+}
+
+/* function updateName() {
+let name = document.getElementById("updateName").value;
+} */
